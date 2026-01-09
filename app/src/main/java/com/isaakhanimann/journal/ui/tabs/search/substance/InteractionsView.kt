cd2fb7c164fee2cd561e38c13b22d3a25c831603
洛铃 <teawhat2000@gmail.com>
2026-01-10 01:05:42 +0800

    Fix missing imports for build

--- app/src/main/java/com/isaakhanimann/journal/ui/tabs/search/substance/InteractionsView.kt
+++ app/src/main/java/com/isaakhanimann/journal/ui/tabs/search/substance/InteractionsView.kt
@@ -24,6 +24,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Info
