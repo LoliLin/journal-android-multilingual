85c6dc019f56db59b32ec9f9f576241ec8f068ed
洛铃 <teawhat2000@gmail.com>
2026-01-10 02:07:24 +0800

    Update applicationId to in.kawaiis.journal

--- app/src/androidTest/java/com/isaakhanimann/journal/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/isaakhanimann/journal/ExampleInstrumentedTest.kt
@@ -35,6 +35,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.isaakhanimann.journal", appContext.packageName)
+        assertEquals("in.kawaiis.journal", appContext.packageName)
     }
 }
