903674125f948a39e4ee2f75d959f2c060d63daa
洛铃 <teawhat2000@gmail.com>
2026-06-09 09:49:51 +0800

    fix: CircleShape import path
    - foundation.CircleShape -> foundation.shape.CircleShape

--- app/src/main/java/com/isaakhanimann/journal/ui/tabs/stats/StatsScreen.kt
+++ app/src/main/java/com/isaakhanimann/journal/ui/tabs/stats/StatsScreen.kt
@@ -66,7 +66,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.compose.foundation.CircleShape
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
