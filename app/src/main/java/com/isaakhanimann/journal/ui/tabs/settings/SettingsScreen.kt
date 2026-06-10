287db17cbc2d5d63fb2aec0876a6387540d09e6b
洛铃 <teawhat2000@gmail.com>
2026-06-11 05:09:31 +0800

    Restore copyright notice and clean up code

--- app/src/main/java/com/isaakhanimann/journal/ui/tabs/settings/SettingsScreen.kt
+++ app/src/main/java/com/isaakhanimann/journal/ui/tabs/settings/SettingsScreen.kt
@@ -100,6 +100,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.isaakhanimann.journal.ui.tabs.settings.AvatarUtil
 import java.io.File
 
