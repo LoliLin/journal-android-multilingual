89aa64eceed8d84aee85e717c4f432a87dfb242c
洛铃 <teawhat2000@gmail.com>
2026-06-11 15:05:01 +0800

    try

--- app/src/main/java/com/isaakhanimann/journal/ui/main/navigation/routers/TabRouter.kt
+++ app/src/main/java/com/isaakhanimann/journal/ui/main/navigation/routers/TabRouter.kt
@@ -24,6 +24,11 @@ import androidx.compose.material.icons.outlined.Book
 import androidx.compose.material.icons.outlined.HealthAndSafety
 import androidx.compose.material.icons.outlined.Medication
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.filled.BarChart
+import androidx.compose.material.icons.filled.Book
+import androidx.compose.material.icons.filled.HealthAndSafety
+import androidx.compose.material.icons.filled.Medication
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class TabRouter(
