f86ca5b114a0ae91131e789e54253ecbca4bf3e4
洛铃 <teawhat2000@gmail.com>
2026-06-12 18:27:57 +0800

    Update ExperienceRow.kt

--- app/src/main/java/com/isaakhanimann/journal/ui/tabs/journal/components/ExperienceRow.kt
+++ app/src/main/java/com/isaakhanimann/journal/ui/tabs/journal/components/ExperienceRow.kt
@@ -105,6 +105,12 @@ fun ExperienceRow(
         val ingestions = experienceWithIngestionsCompanionsAndRatings.ingestionsWithCompanions
         val experience = experienceWithIngestionsCompanionsAndRatings.experience
         val timedNotes = rowViewModel.getTimedNotes(experience.id).collectAsState(initial = emptyList()).value
+        val substanceRepo = rowViewModel.substanceRepo
+        val interactionChecker = rowViewModel.interactionChecker
+        val getSubstanceDisplayName = rowViewModel.substanceRepo::getDisplayName
+        val achievements = rowViewModel.achievementsFlow.collectAsState().value
+
+        
         val cardData = prepareShareableExperienceCardData(
                                 substanceRepo = substanceRepo,
                                 interactionChecker = interactionChecker,
@@ -186,11 +192,6 @@ fun ExperienceRow(
                 }
             }
         }
-
-        val substanceRepo = rowViewModel.substanceRepo
-        val interactionChecker = rowViewModel.interactionChecker
-        val getSubstanceDisplayName = rowViewModel.substanceRepo::getDisplayName
-        val achievements = rowViewModel.achievementsFlow.collectAsState().value
         IconButton(onClick = {
             coroutineScope.launch {
                 val activity = context as? androidx.activity.ComponentActivity
